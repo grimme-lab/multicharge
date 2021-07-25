182cae53eac20c406fba9a312dfb07883f074477
Sebastian Ehlert <28669218+awvwgk@users.noreply.github.com>
2021-07-25 15:31:54 +0200

    Update dependency handling logic in meson and CMake
    - allow meson to find dependencies before falling back to subprojects
    - add custom CMake finder for mctc-lib, mstore and LAPACK
    - add CMake macro to find (cmake/pkgconf) or include (subproject/fetch) dependencies
    - LAPACK/BLAS finder export respective targets for older CMake versions

--- config/template.cmake
+++ config/template.cmake
@@ -1,6 +1,7 @@
 @PACKAGE_INIT@
 
 set("@PROJECT_NAME@_WITH_OpenMP" @WITH_OpenMP@)
+list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}")
 
 if(NOT TARGET "@PROJECT_NAME@::@PROJECT_NAME@")
   include("${CMAKE_CURRENT_LIST_DIR}/@PROJECT_NAME@-targets.cmake")
@@ -13,5 +14,9 @@ if(NOT TARGET "@PROJECT_NAME@::@PROJECT_NAME@")
 
   if(NOT TARGET "LAPACK::LAPACK")
     find_dependency("LAPACK")
+  endif()
+
+  if(NOT TARGET "mctc-lib::mctc-lib")
+    find_dependency("mctc-lib")
   endif()
 endif()
