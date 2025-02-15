bf742887b5995289def0f76c537327b39859eae2
Marvin Friede <51965259+marvinfriede@users.noreply.github.com>
2025-02-15 13:53:50 +0100

    Bump version to 0.3.1 (#43)

--- src/multicharge/version.f90
+++ src/multicharge/version.f90
@@ -23,10 +23,10 @@ module multicharge_version
 
 
    !> String representation of the multicharge version
-   character(len=*), parameter :: multicharge_version_string = "0.3.0"
+   character(len=*), parameter :: multicharge_version_string = "0.3.1"
 
    !> Numeric representation of the multicharge version
-   integer, parameter :: multicharge_version_compact(3) = [0, 3, 0]
+   integer, parameter :: multicharge_version_compact(3) = [0, 3, 1]
 
 
 contains
