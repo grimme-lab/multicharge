cbb1912c65ee7140f23dc70a820d9f3b68ecbe39
thfroitzheim <92028749+thfroitzheim@users.noreply.github.com>
2025-03-19 17:08:41 +0100

    Fix cmake

--- config/cmake/Findmctc-lib.cmake
+++ config/cmake/Findmctc-lib.cmake
@@ -16,7 +16,7 @@
 set(_lib "mctc-lib")
 set(_pkg "MCTCLIB")
 set(_url "https://github.com/grimme-lab/mctc-lib")
-set(_rev "head")
+set(_rev "HEAD")
 
 if(NOT DEFINED "${_pkg}_FIND_METHOD")
   if(DEFINED "${PROJECT_NAME}-dependency-method")
