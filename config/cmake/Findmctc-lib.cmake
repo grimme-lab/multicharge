562bbcc9a5369a540d24db73f32a4b99630574cf
thfroitzheim <92028749+thfroitzheim@users.noreply.github.com>
2025-11-10 13:51:47 +0100

    pin mctc-lib version

--- config/cmake/Findmctc-lib.cmake
+++ config/cmake/Findmctc-lib.cmake
@@ -15,8 +15,8 @@
 
 set(_lib "mctc-lib")
 set(_pkg "MCTCLIB")
-set(_url "https://github.com/thfroitzheim/mctc-lib")
-set(_rev "update-jonquil")
+set(_url "https://github.com/grimme-lab/mctc-lib")
+set(_rev "v0.5.1")
 
 if(NOT DEFINED "${_pkg}_FIND_METHOD")
   if(DEFINED "${PROJECT_NAME}-dependency-method")
