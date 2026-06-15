431a99e448cfd4b32a56a914e478bd249c4971a4
Sebastian Ehlert <sehlert@microsoft.com>
2026-06-15 17:03:45 +0200

    Update export

--- config/template-config.cmake
+++ config/template-config.cmake
@@ -7,8 +7,8 @@ set(MULTICHARGE_USE_MCTCLIB @MULTICHARGE_USE_MCTCLIB@)
 enable_language("Fortran")
 
 if(NOT TARGET "@PROJECT_NAME@::@PROJECT_NAME@")
-  include("${CMAKE_CURRENT_LIST_DIR}/@PROJECT_NAME@-targets.cmake")
   include(CMakeFindDependencyMacro)
+  list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}")
 
   if(NOT TARGET "OpenMP::OpenMP_Fortran" AND MULTICHARGE_WITH_OpenMP)
     find_dependency("OpenMP")
@@ -22,4 +22,6 @@ if(NOT TARGET "@PROJECT_NAME@::@PROJECT_NAME@")
     find_dependency("mctc-lib")
   endif()
 
+  list(REMOVE_AT CMAKE_MODULE_PATH -1)
+  include("${CMAKE_CURRENT_LIST_DIR}/@PROJECT_NAME@-targets.cmake")
 endif()
